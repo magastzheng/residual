66b372f3fe4f06835294d9e7166dee6aceeebe4d
Zheng Guoqiang <zgq10722@htsec.com>
2017-03-17 18:11:16 +0800

    add the test case

--- residual/residtest.py
+++ residual/residtest.py
@@ -10,44 +10,8 @@ import statsmodels.api as sm
 import dataapi
 import os
 import pdb
-
-indusColumns=['Indus_1','Indus_2','Indus_3','Indus_4','Indus_5','Indus_6','Indus_7','Indus_8','Indus_9','Indus_10','Indus_11','Indus_12','Indus_13','Indus_14','Indus_15','Indus_16','Indus_17','Indus_18','Indus_19','Indus_20','Indus_21','Indus_22','Indus_23','Indus_24','Indus_25','Indus_26','Indus_27','Indus_28','Indus_29']
-
-def calcOneFactor(df, newIndusColumns, column, nmmv):
-		"""	df - pandas DataFrame对象，为某天标准化后的因子数据
-			newIndusColumns - 行业因子名称列表
-			column - 当前因子名称，因变量
-			nmmv - 自由流通市值名称，自变量
-			
-			return -  pandas Series对象表示的残差
-		"""
-		#TODO: 如果是流通市值，不需要再跟自己回归
-		#allCols = list(newIndusColumns)
-		#if column == nmmv:
-		#	pass
-		#else:
-		#	allCols.insert(0, nmmv)
-		
-		df = df.fillna(0)
-		
-		#raise Exception
-		#pdb.set_trace()
-		print 'hello'
-		#X = sm.add_constant(df[nmmv])
-		#if column == nmmv:
-			#remove  column
-		#	X=X.drop([nmmv], axis=1)
-		X = df[nmmv]
-		X = pd.concat([X[:], df[newIndusColumns]], axis=1)
-		
-		Y = df[column]
-		#model = sm.OLS(df[column], df.loc[:,allCols])
-		model = smf.OLS(Y, X)
-		results = model.fit()
-		raise Exception
-		#print results.summary()
-		print results.resid.head()
-		return results.resid
+import residual
+import fileutil
 
 def appendIndustryColumn(df, industryColumns):
 		"""
@@ -100,14 +64,6 @@ def getIndustryColumnName(industries):
 				indusColumns.append(name)
 		return indusColumns
 
-#def getIndustryData(industries, industry):
-#	arr = np.zeros(len(industries))
-#	inlist = industries.tolist()
-#	if industry in inlist:
-#			idx = inlist.index(industry)
-#			arr[idx] = 1
-#	return arr
-
 def preprocessData(df):
 	"""	df - pandas DataFrame对象，其中数据是已经标准化之后的
 		return - 新列名，list类型
@@ -124,60 +80,47 @@ def preprocessData(df):
 	
 	return newIndusColumns
 
-def getOneDayResid(df, newIndusColumns,  keyCols, includeCols, excludeCols):		
-	"""	df - pandas DataFrame对象，df中的数据已经将没有行业的行去除掉，并添加行业列，如果属于该行业，则本列为1，否则为0
-		keyCols - 新的pandas DataFrame对象中，必须保留的列，list类型
-		includeCols - 需要处理的列名，list类型
-		excludeCols - 不需要处理的列名，list类型
-
-		return - pandas DataFrame对象，包含需要处理的列残差
+def calcResidual(df, keyCols, includeCols, excludeCols):
+	"""	计算残差
+		td - 交易日
+		df - td那个交易日的标准化数据，pandas DataFrame对象
+		keyCols - 最终结果中必须出现的列名列表
+		includeCols - 必须计算残差的列名列表
+		excludeCols - 不需要计算残差的列名列表
 	"""
-	columns = df.columns
-	newdf = df.loc[:, keyCols]
-
-	for column in columns:
-		if column in includeCols:
-			print column
-			resid = calcOneFactor(df, newIndusColumns, column, 'NonRestrictedCap')
-			newdf[column] = np.nan
-			for idx in resid.index:
-					newdf.loc[idx, column] = resid[idx]
-			break
-		elif column in excludeCols:
-			print 'Skip: {0}'.format(column)
-		else:
-			print 'Cannot support: {0}'.format(column)
 	
+	#去掉行业为空的行
+	print 'preprocess'
+	df = df.dropna(subset=['IndustrySecuCode_I'])
+	newIndusColumns = preprocessData(df)
+	
+	print 'calculate ....'
+	start = datetime.datetime.now()
+
+	newdf = residual.getOneDayResid(df, newIndusColumns, keyCols, includeCols, excludeCols)
+	
+	end = datetime.datetime.now()
+	
+	print 'cost: {0}'.format(end-start)
 	return newdf
 
-def saveOneDay(filepath, td, df):
-	""" filepath - 保存文件的路径名，为str类型
-		td - 交易日，为datetime.date类型
-		df - pandas DataFrame对象
-	"""
-	df['SecuAbbr'] = df['SecuAbbr'].apply(lambda x:x.decode('gbk'))
-
-	filename = td.strftime('%Y%m%d')
-	fullpath='{0}{1}.csv'.format(filepath, filename)
-	#raise Exception
-	df.to_csv(fullpath, encoding='gbk')
-
 def test():
-	td = datetime.date(2017, 3, 8)
-	stdPath = 'D:/workspace/python/residual/result/'
+	td = datetime.date(2017, 3, 16)
+	stdPath = 'D:/workspace/python/residual/result/monthly/'
 	residPath = 'D:/workspace/python/residual/residtest/'
 	#csvpath = '{0}{1}.csv'.format(stdPath, td.strftime('%Y%m%d'))
-	csvpath = '{0}{1}.csv'.format(stdPath, '20170308-r')
+	
 	keyCols = dataapi.keyCols
 	includeCols = dataapi.includeCols
 	excludeCols = dataapi.excludeCols
 	newIndusColumns = indusColumns
-	if os.path.exists(csvpath):
-		df=pd.read_csv(csvpath)
+	
+	df = fileutil.loadPickle(stdPath, td)
+	if df is not None:
 		#去掉行业为空的行
-		df = df.dropna(subset=['IndustrySecuCode_I'])
+		#df = df.dropna(subset=['IndustrySecuCode_I'])
 		#newIndusColumns = preprocessData(df)
-		newdf = getOneDayResid(df, newIndusColumns, keyCols, includeCols, excludeCols)
+		newdf = calcResidual(df, keyCols, includeCols, excludeCols)
 	else:
 		pass
 
